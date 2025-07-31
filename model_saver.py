2cf777e2910b77ea75f25af8fcf1cfbefad57653
Toshan Kirit Jagani <155760271+tkC0ding@users.noreply.github.com>
2025-07-31 00:48:43 -0400

    JUSST GIT PUSH ALREADY

--- model_saver.py
+++ model_saver.py
@@ -1,9 +1,19 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from peft import PeftModel
 
+base_model_name = "distilbert-base-uncased"
+lora_checkpoint_path = "results/checkpoint-2560"
 label2id = {"positive" : 1, "negative" : 0, "neutral" : 2}
 id2label = {0 : "negative", 1 : "positive", 2 : "neutral"}
-model = AutoModelForSequenceClassification.from_pretrained("results/checkpoint-2560", num_labels=3, label2id=label2id, id2label=id2label)
-tokenizer = AutoTokenizer.from_pretrained("results/checkpoint-2560")
 
-model.save_pretrained("final_model")
-tokenizer.save_pretrained("final_model")
+base_model = AutoModelForSequenceClassification.from_pretrained(base_model_name, num_labels=3, label2id=label2id, id2label=id2label)
+tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+
+model = PeftModel.from_pretrained(base_model, lora_checkpoint_path)
+
+model = model.merge_and_unload()
+
+output_dir = "final_model"
+model.save_pretrained(output_dir)
+model.config.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
